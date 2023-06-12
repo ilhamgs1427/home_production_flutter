09f32602bf3cd2bbc51554b839095bb816d22f5e
ilhamgs1427 <pp3583316@gmail.com>
2023-06-13 00:32:09 +0700

    final

--- lib/Network/Api/url_api.dart
+++ lib/Network/Api/url_api.dart
@@ -1,10 +1,10 @@
 class BASEURL {
-  static String ipAddress = "192.168.0.107";
-  static String apiRegister = "http://$ipAddress:8000/api/register";
-  static String apiLogin = "http://$ipAddress:8000/api/login";
-  static String getProduct = "http://$ipAddress:8000/api/getproduct";
-  static String checkout = "http://$ipAddress:8000/api/checkout";
-  static String history = "http://$ipAddress:8000/api/gethistory/";
-  static String transfer = "http://$ipAddress:8000/api/buktitransfer";
-  static String deleteRiwayat = "http://$ipAddress:8000/api/deleteriwayat";
+  static String ipAddress = "bsmagro.com";
+  static String apiRegister = "http://$ipAddress/api/register";
+  static String apiLogin = "http://$ipAddress/api/login";
+  static String getProduct = "http://$ipAddress/api/getproduct";
+  static String checkout = "http://$ipAddress/api/checkout";
+  static String history = "http://$ipAddress/api/gethistory/";
+  static String transfer = "http://$ipAddress/api/buktitransfer";
+  static String deleteRiwayat = "http://$ipAddress/api/deleteriwayat";
 }
